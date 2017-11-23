627e77c949acd3b2de43c45d7b05ed66361cb5e6
Steven <stevelee2497@gmail.com>
2017-11-23 20:54:57 +0700

    refactor code

--- ex2solution/Program.cs
+++ ex2solution/Program.cs
@@ -5,23 +5,24 @@ namespace ex2solution
 {
     class Program
     {
-        static List<int> OddNumbers(int number)
-        {
-            var oddnumbers = new List<int>();
-            for (var i = 1; i < number; i += 2)
-            {
-                oddnumbers.Add(i);
-            }
-            return oddnumbers;
-        }
-
         static void Main(string[] args)
         {
-            Func<int, List<int>> createList = OddNumbers;
+            Func<int, Func<int, bool>, List<int>> InitArray = (n, method) =>
+            {
+                var list = new List<int>();
+                for (var i = 1; i < n; i++)
+                {
+                    if (method(i))
+                    {
+                        list.Add(i);
+                    }
+                }
+                return list;
+            };
             Console.Write("Enter a number : ");
             var number = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine($"Odd number below {number} :");
-            var oddnumbers = createList(number);
+            var oddnumbers = InitArray(number, x => x % 2 == 1);
             oddnumbers.ForEach(Console.WriteLine);
             Console.ReadKey();
         }
