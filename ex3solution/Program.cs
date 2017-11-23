ba0016f779c6a7f3b91094f56b9e425c8b55e2d5
Steven <stevelee2497@gmail.com>
2017-11-23 20:49:51 +0700

    add ex 3 solution

---
+++ ex3solution/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ex3solution
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<int, Func<int, long>, long> sum = (n, method) =>
+            {
+                var sumResult = 0L;
+                for (var i = 1; i <= n; i++)
+                {
+                    sumResult += method(i);
+                }
+                return sumResult;
+            };
+            var s = sum(5, x => (long) Math.Pow(x, x));
+            Console.WriteLine(s);
+            Console.ReadKey();
+        }
+    }
+}
