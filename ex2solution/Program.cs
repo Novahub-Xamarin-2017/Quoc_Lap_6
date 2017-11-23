1bdb478a068134ec0bb9d4a7e60884d61d15c379
Steven <stevelee2497@gmail.com>
2017-11-23 21:14:26 +0700

    extract function

--- ex2solution/Program.cs
+++ ex2solution/Program.cs
@@ -5,20 +5,20 @@ namespace ex2solution
 {
     class Program
     {
+        private static List<int> InitArray(int n, Func<int, bool> method)
+        {
+            var list = new List<int>();
+            for (var i = 1; i < n; i++)
+            {
+                if (method(i))
+                {
+                    list.Add(i);
+                }
+            }
+            return list;
+        }
         static void Main(string[] args)
         {
-            Func<int, Func<int, bool>, List<int>> InitArray = (n, method) =>
-            {
-                var list = new List<int>();
-                for (var i = 1; i < n; i++)
-                {
-                    if (method(i))
-                    {
-                        list.Add(i);
-                    }
-                }
-                return list;
-            };
             Console.Write("Enter a number : ");
             var number = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine($"Odd number below {number} :");
