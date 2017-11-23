ee30656535fa68cc9269ddb041c35e36a5979436
Steven <stevelee2497@gmail.com>
2017-11-23 20:12:10 +0700

    add ex 2 solution

---
+++ ex2solution/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ex2solution
+{
+    class Program
+    {
+        static List<int> OddNumbers(int number)
+        {
+            var oddnumbers = new List<int>();
+            for (var i = 1; i < number; i += 2)
+            {
+                oddnumbers.Add(i);
+            }
+            return oddnumbers;
+        }
+
+        static void Main(string[] args)
+        {
+            Func<int, List<int>> createList = OddNumbers;
+            Console.Write("Enter a number : ");
+            var number = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Odd number below {number} :");
+            var oddnumbers = createList(number);
+            oddnumbers.ForEach(Console.WriteLine);
+            Console.ReadKey();
+        }
+    }
+}
