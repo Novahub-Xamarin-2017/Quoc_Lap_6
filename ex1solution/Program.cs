d6f84a0ef2761f4b563000d9af57624c3b4ff70a
Steven <stevelee2497@gmail.com>
2017-11-23 19:57:01 +0700

    add exercise 1 solution : Prime numbers

---
+++ ex1solution/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ex1solution
+{
+    class Program
+    {
+        static List<int> PrimeNumbers(int n)
+        {
+            List<int> primeNumbers = new List<int>();
+            for (int i = 2; i < n; i++)
+            {
+                if (i.IsPrme())
+                {
+                    primeNumbers.Add(i);
+                }
+            }
+            return primeNumbers;
+        }
+        static void Main(string[] args)
+        {
+            Func<int, List<int>> createList = PrimeNumbers;
+            createList(100).ForEach(Console.WriteLine);
+            Console.ReadKey();
+        }
+    }
+}
